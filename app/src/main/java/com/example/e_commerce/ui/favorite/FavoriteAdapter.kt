362194f43f790afe10c42c2e68c83227f44c0bc5
Yusuf Orhan <yazilimcigenc2@gmail.com>
2023-11-18 18:14:18 +0300

    create favorite adapter

---
+++ app/src/main/java/com/example/e_commerce/ui/favorite/FavoriteAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.e_commerce.ui.favorite
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.e_commerce.common.loadImage
+import com.example.e_commerce.data.model.retrofit.ProductsItem
+import com.example.e_commerce.data.model.room.FavoriteModel
+import com.example.e_commerce.databinding.FavoriteListItemBinding
+
+class FavoriteAdapter : RecyclerView.Adapter<FavoriteAdapter.FavoriteViewHolder>() {
+    val list = arrayListOf<FavoriteModel>()
+    class FavoriteViewHolder(val binding : FavoriteListItemBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(productsItem: ProductsItem,context: Context){
+            with(binding){
+                favoriteImage.loadImage(productsItem.image,context)
+                descriptionText.text = productsItem.description
+                priceText.text = "${productsItem.price}$"
+                rateText.text = productsItem.rating.rate.toString()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteViewHolder {
+        val binding = FavoriteListItemBinding.inflate(LayoutInflater.from(parent.context))
+        return FavoriteViewHolder(binding)
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: FavoriteViewHolder, position: Int) {
+        holder.bind(list[position].productsItem,holder.itemView.context)
+    }
+}
